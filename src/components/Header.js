0aa0329e7dc3cf141ec72f0a782e8e5b95196b2d
Jasoncc24 <77653071+Jasoncc24@users.noreply.github.com>
2023-01-11 21:45:41 -0500

    centered header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,30 +8,28 @@ import MobileNav from "../components/MobileNav";
 import Nav from "../components/Nav";
 
 const Header = () => {
-  //mobile nav state
+  // mobile nav state
   const [mobileNav, setMobileNav] = useState(false);
   // header state
   const [isActive, setIsActive] = useState(false);
   // destructure header data
   const { logo, btnText } = header;
-
-  //scroll event
+  // scroll event
   useEffect(() => {
     window.addEventListener("scroll", () => {
       window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
     });
   });
-
   return (
     <header
       className={`${
         isActive ? "lg:top-0 bg-white shadow-2xl" : "lg:top-[60px]"
       } py-6 lg:py-4 fixed w-full transition-all z-10`}
     >
-      <div className="container flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
-        <a href="/" data-aos="fade-down" data-aos-delay="1000">
-          <img src={logo} alt="logo" />
+        <a href="#" data-aos="fade-down" data-aos-delay="1000">
+          <img src={logo} alt="" />
         </a>
         {/* nav - initially hidden - show on desktop mode */}
         <div
